179bc0dfdb8e7ea8445b6044290c1237ad101f34
Kelvin Atawura <katawura@gmail.com>
2024-05-12 17:49:28 +0100

    Protected routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', Controllers\HomeController::class)->name('home');
 
 Route::get('about', Controllers\AboutController::class)->name('about');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', Controllers\DashboardController::class)->name('dashboard');
 
@@ -14,17 +15,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::patch('profile', 'update')->name('profile.update');
         Route::delete('profile', 'destroy')->name('profile.destroy');
     });
+
+    Route::resource('clients', App\Http\Controllers\ClientController::class);
+    Route::resource('projects', App\Http\Controllers\ProjectController::class);
+    Route::resource('timers', App\Http\Controllers\TimerController::class);
+    Route::resource('reports', App\Http\Controllers\ReportController::class);
 });
 
 require __DIR__ . '/features.php';
 require __DIR__ . '/auth.php';
 require __DIR__ . '/dev.php';
-
-
-Route::resource('clients', App\Http\Controllers\ClientController::class);
-
-Route::resource('projects', App\Http\Controllers\ProjectController::class);
-
-Route::resource('timers', App\Http\Controllers\TimerController::class);
-
-Route::resource('reports', App\Http\Controllers\ReportController::class);
