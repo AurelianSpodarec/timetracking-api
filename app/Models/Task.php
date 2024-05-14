1c84babc87c78c380ef92f8b03462f0e74a8da68
Kelvin Atawura <katawura@gmail.com>
2024-05-14 11:31:00 +0100

    Removed unneed code

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,7 +9,6 @@ class Task extends Model
 {
     use HasFactory;
 
-
     /**
      * The attributes that are mass assignable.
      *
--- resources/js/pages/client/index.tsx
+++ resources/js/pages/client/index.tsx
@@ -1,7 +1,1 @@
-{{--
-    @extends('layouts.app')
 
-    @section('content')
-        client.index template
-    @endsection
---}}
